5690206bc7611065d19e5465e478bcd05a6252f4
Avyansh Ralph <avyansh.ralph@hotmail.com>
2021-10-23 22:03:52 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class extends Document {
           <meta charSet="utf-8" />
           <link rel="icon" type="image/png" href="https://avatars3.githubusercontent.com/u/25351731?s=460&v=4" />
           <meta name="theme-color" content="#ffe8e8" />
-          <meta name="description" content="Alistair Smith, Full–stack TypeScript engineer from the UK" />
+          <meta name="description" content="Avyansh Ralph" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Space+Mono:wght@700&display=swap"

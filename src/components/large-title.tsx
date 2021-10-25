a3a409639913b119c0876dfbce0d3d01583a8351
Avyansh Ralph <avyansh.ralph@hotmail.com>
2021-10-25 21:10:59 +0530

    Delete src directory

--- src/components/large-title.tsx
+++
@@ -1,5 +0,0 @@
-import {ReactNode} from 'react';
-
-export function LargeTitle({children}: {children: ReactNode}) {
-  return <h1 className="title text-6xl font-bold">{children}</h1>;
-}
--- src/core/data.ts
+++
@@ -1,1 +0,0 @@
-export const initialBackground = 'https://source.unsplash.com/tHtZo3FLhPc/1920x1080';

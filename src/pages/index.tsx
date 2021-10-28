6c7314d025282f316ed690f5a847fa8ef3fca48d
Avyansh Ralph <avyansh.ralph@hotmail.com>
2021-10-28 20:08:39 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
   });
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/Avyansh0001/repos`)
+    fetch(`https://api.github.com/users/Avyansh0001repos`)
       .then((res) => res.json())
       .then((repos) => {
         setRepo({ repos: repos });

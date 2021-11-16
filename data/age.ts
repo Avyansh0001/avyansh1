414a39b970ad37d43cc30454c9bc76c71a35c0e2
Avyansh Ralph <avyansh.ralph@hotmail.com>
2021-11-16 10:18:34 +0530

    Delete data directory

--- data/age.ts
+++
@@ -1,6 +0,0 @@
-// thanks ali <3
-import day from "dayjs";
-const birthday = day("13 March 2008").toDate();
-export const age = Math.abs(
-  new Date(Date.now() - birthday.getTime()).getUTCFullYear() - 1970
-);
